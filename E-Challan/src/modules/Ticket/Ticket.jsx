980d4b4ec0ece4eb2ef456291e18e25e870c9f3c
Tanush-Kaushik <141478739+Tanush-Kaushik@users.noreply.github.com>
2024-02-19 10:48:36 +0530

    Update Ticket.jsx

--- E-Challan/src/modules/Ticket/Ticket.jsx
+++ E-Challan/src/modules/Ticket/Ticket.jsx
@@ -71,7 +71,7 @@ const Ticket = () => {
                   type="text"
                   ref={vehicleNumberRef}
                   id="simple-search"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5   bg-gray-700  border-gray-600  placeholder-gray-400  text-white  focus:ring-blue-500  focus:border-blue-500"
+                  className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5   bg-gray-700  border-gray-600  placeholder-gray-400  text-white  focus:ring-blue-500  focus:border-blue-500"
                   placeholder="Enter vehicle number"
                   required
                 />
